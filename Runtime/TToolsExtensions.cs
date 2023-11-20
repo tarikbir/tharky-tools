628a0bab69a6b8b1190d92197303511de7117cd9
Tarık Bir <7569835+tarikbir@users.noreply.github.com>
2023-11-20 18:10:07 +0300

    Update TToolsExtensions.cs

--- Runtime/TToolsExtensions.cs
+++ Runtime/TToolsExtensions.cs
@@ -10,27 +10,94 @@ namespace TTools
     {
         private static readonly StringBuilder _sb = new();
 
+        #region GameObject
+        /// <summary>Gets a component or if the component is non-existent, creates one and adds it onto the game object.</summary>
+        /* By adammyhre (git-amend) */
+        public static T GetOrAdd<T>(this GameObject gameObject) where T : Component
+        {
+            T component = gameObject.GetComponent<T>();
+            if (!component) component = gameObject.AddComponent<T>();
+            return component;
+        }
+
+        /// <summary>Allows Unity objects to be used for null propogation by returning null if the object is <null>.</summary>
+        /* By adammyhre (git-amend) */
+        public static T OrNull<T>(this T obj) where T : UnityEngine.Object => obj ? obj : null;
+        #endregion
+
         #region Transform
+        /// <summary>Resets the transform back to its default values.</summary>
+        public static void Reset(this Transform transform)
+        {
+            transform.position = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+        }
+
         /// <summary>Destroys all children of a parent. (Sounds horrible, I know)</summary>
         /* By Tarodev */
-        public static void DestroyChildren(this Transform t)
+        public static void DestroyChildren(this Transform parent)
         {
-            for (int i = t.childCount; i > 0; --i)
-                GameObject.Destroy(t.GetChild(0).gameObject);
+            for (int i = parent.childCount; i > 0; --i)
+                GameObject.Destroy(parent.GetChild(0).gameObject);
         }
 
         /// <summary>Destroys all children of a parent immediately. (Yuck)</summary>
         /* By Tarodev */
-        public static void DestroyChildrenImmediate(this Transform t)
+        public static void DestroyChildrenImmediate(this Transform parent)
         {
-            for (int i = t.childCount; i > 0; --i)
-                GameObject.DestroyImmediate(t.GetChild(0).gameObject);
+            for (int i = parent.childCount; i > 0; --i)
+                GameObject.DestroyImmediate(parent.GetChild(0).gameObject);
+        }
+
+        /// <summary>Operate on children of a transform. From last to first.</summary>
+        /* By adammyhre (git-amend) */
+        public static void ForEveryChild(this Transform parent, System.Action<Transform> action)
+        {
+            for (var i = parent.childCount - 1; i >= 0; i--)
+            {
+                action(parent.GetChild(i));
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all the children of a given Transform.
+        /// </summary>
+        /// <remarks>
+        /// This method can be used with LINQ to perform operations on all child Transforms. For example,
+        /// you could use it to find all children with a specific tag, to disable all children, etc.
+        /// Transform implements IEnumerable and the GetEnumerator method which returns an IEnumerator of all its children.
+        /// </remarks>
+        /// <param name="parent">The Transform to retrieve children from.</param>
+        /// <returns>An IEnumerable&lt;Transform&gt; containing all the child Transforms of the parent.</returns>    
+        /* By adammyhre (git-amend) */
+        public static IEnumerable<Transform> Children(this Transform parent)
+        {
+            foreach (Transform child in parent)
+            {
+                yield return child;
+            }
+        }
+        #endregion
+
+        #region Vectors
+        /// <summary>
+        /// Returns a Boolean indicating whether the current Vector3 is in a given range from another Vector3
+        /// </summary>
+        /// <param name="current">The current Vector3 position</param>
+        /// <param name="target">The Vector3 position to compare against</param>
+        /// <param name="range">The range value to compare against</param>
+        /// <returns>True if the current Vector3 is in the given range from the target Vector3, false otherwise</returns>
+        /* By adammyhre (git-amend) */
+        public static bool IsInRangeOf(this Vector3 current, Vector3 target, float range)
+        {
+            return (current - target).sqrMagnitude <= range * range;
         }
         #endregion
 
         #region Arrays
         /// <summary>Converts a list to string with a Comma Delimited List of Values.</summary>
-		/* By Jason Storey */
+        /* By Jason Storey */
         public static string ToReadable<T>(this IEnumerable<T> source)
         {
             if (source == null) return "null";
@@ -144,11 +211,25 @@ namespace TTools
         {
             return random.Next(1, 100) <= chance;
         }
+
+        /// <summary>
+        /// Converts an IEnumerator<T> to an IEnumerable<T>.
+        /// </summary>
+        /// <param name="e">An instance of IEnumerator<T>.</param>
+        /// <returns>An IEnumerable<T> with the same elements as the input instance.</returns>
+        /* By adammyhre (git-amend) */
+        public static IEnumerable<T> ToIEnumerable<T>(this IEnumerator<T> enumerator)
+        {
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
         #endregion
     }
 
     /* By Nick Chapsas */
-    public ref struct CustomIntEnumerator
+        public ref struct CustomIntEnumerator
     {
         private int _current;
         private readonly int _end;
